05e62daf6d6d2f0bb1949cc7507279273bd947a6
zliel <zpliel@gmail.com>
2026-04-15 22:21:20 -0700

    Added a loading state for when clerk is loading

--- src/integrations/clerk/header-user.tsx
+++ src/integrations/clerk/header-user.tsx
@@ -1,18 +1,26 @@
 import {
   SignInButton,
   UserButton,
-  Show
+  Show,
+  ClerkLoading,
+  ClerkLoaded
 } from '@clerk/tanstack-react-start'
+import { Skeleton } from '#/components/ui/skeleton';
 
 export default function HeaderUser() {
   return (
-    <>
-      <Show when="signed-in">
-        <UserButton />
-      </Show>
-      <Show when="signed-out">
-        <SignInButton />
-      </Show>
-    </>
+    <div className="h-7 w-7">
+      <ClerkLoading>
+        <Skeleton className="h-7 w-7 animate-pulse rounded-full bg-gray-300" />
+      </ClerkLoading>
+      <ClerkLoaded>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+        <Show when="signed-out">
+          <SignInButton />
+        </Show>
+      </ClerkLoaded>
+    </div>
   )
 }

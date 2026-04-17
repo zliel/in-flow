a9d858ff2c664867251bf677557890781116bf29
zliel <zpliel@gmail.com>
2026-04-17 10:54:02 -0700

    Added a hook for using the clerk + supabase client

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,19 @@
-import { createClient } from "@supabase/supabase-js";
+import { useMemo } from 'react'
+import { useSession } from '@clerk/tanstack-react-start'
+import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
-);
+export function useSupabase() {
+  const { session } = useSession()
+
+  return useMemo(() => {
+    return createClient(
+      import.meta.env.VITE_SUPABASE_URL!,
+      import.meta.env.VITE_SUPABASE_KEY!,
+      {
+        async accessToken() {
+          return session?.getToken() ?? null
+        },
+      }
+    )
+  }, [session])
+}

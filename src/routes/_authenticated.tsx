4c2e3557049757cd83d31925dedf16684ddb5c23
zliel <zpliel@gmail.com>
2026-04-17 11:57:06 -0700

    Added a check to requireAuth that checks if the user has any registered block types or blocks and seeds the database if not

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -2,15 +2,45 @@ import { createFileRoute, Outlet } from '@tanstack/react-router'
 
 import { createServerFn } from '@tanstack/react-start'
 import { auth } from '@clerk/tanstack-react-start/server'
-import { redirect } from "@tanstack/react-router"
+import { redirect } from '@tanstack/react-router'
+import { createClient } from '@supabase/supabase-js'
 
 const requireAuth = createServerFn().handler(async () => {
-  const { isAuthenticated, userId } = await auth()
+  const { isAuthenticated, userId, getToken } = await auth()
 
   if (!isAuthenticated) {
     throw redirect({
       to: '/sign-in/$',
     })
+  }
+
+  // TODO: This is fine for now but will need to be moved to be in the loader for a specific page
+  // to avoid making this request for every single authenticated page
+  const token = await getToken()
+
+  const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL!,
+    import.meta.env.VITE_SUPABASE_KEY!,
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+      },
+    },
+  )
+
+  const { count } = await supabase
+    .from('block_types')
+    .select('*', { count: 'exact', head: true })
+    .limit(1)
+
+  if (count === 0) {
+    await supabase.rpc('seed_default_block_types', { p_user_id: userId })
   }
 
   return { userId }
